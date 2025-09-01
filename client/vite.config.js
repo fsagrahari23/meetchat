f16148117b0d466da42741bd54d998fdc03c63db
fsagrahari23 <monu.a23@iiits.in>
2025-09-01 09:27:50 +0530

    added docker-compose

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        credentials: true,
+export default ({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the prefix.
+  // eslint-disable-next-line no-undef
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return defineConfig({
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL || 'http://localhost:5000',
+          changeOrigin: true,
+          credentials: true,
+        },
       },
     },
-  },
-})
+  })
+}
